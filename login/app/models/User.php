a344eeb8c1db49bb373797c1ed9602d1a47bece8
Shift <shift@laravelshift.com>
2025-12-18 19:04:08 +0000

    Shift cleanup

--- login/app/models/User.php
+++ login/app/models/User.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
-use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Auth\Reminders\RemindableInterface;
 use Illuminate\Auth\UserInterface;
 
