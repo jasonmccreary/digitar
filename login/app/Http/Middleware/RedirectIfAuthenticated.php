3b35caa86a2c0994e9bc1a8553a879db77415917
Shift <shift@laravelshift.com>
2025-12-18 20:40:27 +0000

    Shift middleware
    Laravel 5.2 adjusts the `Guard` object used within middleware. In
    addition, new `can` and `throttles` middleware were added.

--- login/app/Http/Middleware/RedirectIfAuthenticated.php
+++ login/app/Http/Middleware/RedirectIfAuthenticated.php
@@ -3,36 +3,20 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Support\Facades\Auth;
 
 class RedirectIfAuthenticated
 {
     /**
-     * The Guard implementation.
-     *
-     * @var Guard
-     */
-    protected $auth;
-
-    /**
-     * Create a new filter instance.
-     *
-     * @return void
-     */
-    public function __construct(Guard $auth)
-    {
-        $this->auth = $auth;
-    }
-
-    /**
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
+     * @param  string|null  $guard
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle($request, Closure $next, $guard = null)
     {
-        if ($this->auth->check()) {
+        if (Auth::guard($guard)->check()) {
             return redirect('/');
         }
 
