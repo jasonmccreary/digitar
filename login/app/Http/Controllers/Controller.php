a2b33c07a1647a5fc60172f20f1be4890b0a386a
Shift <shift@laravelshift.com>
2025-12-19 17:12:21 +0000

    Use `Gate` facade for controller authorization

--- login/app/Http/Controllers/Controller.php
+++ login/app/Http/Controllers/Controller.php
@@ -2,12 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 abstract class Controller extends BaseController
 {
-    use AuthorizesRequests;
 
     /**
      * Setup the layout used by the controller.
