060db9ba753794171104769e6a9ae854837e4690
Shift <shift@laravelshift.com>
2025-12-19 17:12:21 +0000

    Validate via object directly within Controllers

--- login/app/Http/Controllers/Controller.php
+++ login/app/Http/Controllers/Controller.php
@@ -3,12 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 abstract class Controller extends BaseController
 {
-    use AuthorizesRequests, ValidatesRequests;
+    use AuthorizesRequests;
 
     /**
      * Setup the layout used by the controller.
